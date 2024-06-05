420fe3be6a54ec6eec0551f65c599f2cf5992e0a
ZacharyAHintz <Zackhmail@gmail.com>
2024-06-05 10:28:51 -0500

    last of the lists finished

---
+++ NpcCreator/src/components/lists/MentorSpiritList.jsx
@@ -0,0 +1,137 @@
+export default function MentorSpiritList() {
+  const mentorSpirits = {
+    adversary: {
+      name: "Adversary",
+      similarArchetypes: "Chaos, Anarchy",
+      source: "SR5:HT",
+    },
+    alligator: {
+      name: "Alligator",
+      similarArchetypes: "Crocodile, Sloth, Greed",
+      source: "SR5:HS",
+    },
+    bear: {
+      name: "Bear",
+      similarArchetypes: "Strength, Protection",
+      source: "Core",
+    },
+    berserker: {
+      name: "Berserker",
+      similarArchetypes: "Warrior, Warmonger",
+      source: "SR5:SG",
+    },
+    bat: { name: "Bat", similarArchetypes: "Wanderer", source: "SR5:HT" },
+    boar: { name: "Boar", similarArchetypes: "", source: "SR5:HS" },
+    cat: { name: "Cat", similarArchetypes: "Mystery, Stealth", source: "Core" },
+    chaos: {
+      name: "Chaos",
+      similarArchetypes: "Trickster, Troublemaker",
+      source: "SR5:SG",
+    },
+    dog: {
+      name: "Dog",
+      similarArchetypes: "Friendship, Loyalty",
+      source: "Core",
+    },
+    dolphin: { name: "Dolphin", similarArchetypes: "", source: "SR5:HS" },
+    dragonslayer: {
+      name: "Dragonslayer",
+      similarArchetypes: "Heroism, Adventure",
+      source: "Core",
+    },
+    eagle: {
+      name: "Eagle",
+      similarArchetypes: "Nature, Warding",
+      source: "Core",
+    },
+    fireBringer: {
+      name: "Fire-bringer",
+      similarArchetypes: "Invention, Generosity",
+      source: "Core",
+    },
+    giraffe: { name: "Giraffe", similarArchetypes: "", source: "SR5:HS" },
+    horse: { name: "Horse", similarArchetypes: "", source: "SR5:HS" },
+    monkey: { name: "Monkey", similarArchetypes: "", source: "SR5:HT" },
+    mountain: {
+      name: "Mountain",
+      similarArchetypes: "Endurance, Stubbornness",
+      source: "Core",
+    },
+    oracle: {
+      name: "Oracle",
+      similarArchetypes: "Teacher, Mystic",
+      source: "SR5:SG",
+    },
+    peacemaker: {
+      name: "Peacemaker",
+      similarArchetypes: "Mediator, Pacifist",
+      source: "SR5:SG",
+    },
+    raccoon: { name: "Raccoon", similarArchetypes: "", source: "SR5:HS" },
+    rat: {
+      name: "Rat",
+      similarArchetypes: "Survival, Scavenging",
+      source: "Core",
+    },
+    raven: {
+      name: "Raven",
+      similarArchetypes: "Deception, Mischief",
+      source: "Core",
+    },
+    sea: { name: "Sea", similarArchetypes: "Chaos, Greed", source: "Core" },
+    seducer: {
+      name: "Seducer",
+      similarArchetypes: "Seductress, Temptation",
+      source: "Core",
+    },
+    shark: {
+      name: "Shark",
+      similarArchetypes: "Hunger, Violence",
+      source: "Core",
+    },
+    snake: {
+      name: "Snake",
+      similarArchetypes: "Knowledge, Curiosity",
+      source: "Core",
+    },
+    spider: { name: "Spider", similarArchetypes: "", source: "SR5:HS" },
+    thunderbird: {
+      name: "Thunderbird",
+      similarArchetypes: "Anger, War",
+      source: "Core",
+    },
+    whale: { name: "Whale", similarArchetypes: "", source: "SR5:HS" },
+    wiseWarrior: {
+      name: "Wise Warrior",
+      similarArchetypes: "Duty, Wisdom",
+      source: "Core",
+    },
+    wolf: {
+      name: "Wolf",
+      similarArchetypes: "Hunting, Fellowship",
+      source: "Core",
+    },
+    doomToxic: {
+      name: "Doom (TOXIC)",
+      similarArchetypes: "Destroyer, Reaper",
+      source: "SR5:SG",
+    },
+    diseaseToxic: {
+      name: "Disease (TOXIC)",
+      similarArchetypes: "Pestilent Rat, Contagion",
+      source: "SR5:SG",
+    },
+    mutationToxic: {
+      name: "Mutation (TOXIC)",
+      similarArchetypes: "Mutant Beast, Dark Darwinist",
+      source: "SR5:SG",
+    },
+    pollutionToxic: {
+      name: "Pollution (TOXIC)",
+      similarArchetypes: "",
+      source: "SR5:SG",
+    },
+  };
+
+  return mentorSpirits;
+}
