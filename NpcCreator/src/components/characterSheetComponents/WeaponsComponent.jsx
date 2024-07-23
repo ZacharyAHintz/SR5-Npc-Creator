61bc893ef01e5db8cfbd98d2a9ad9d7ae48539e6
ZacharyAHintz <Zackhmail@gmail.com>
2024-07-23 17:11:30 -0500

    flattened guns

--- NpcCreator/src/components/characterSheetComponents/WeaponsComponent.jsx
+++ NpcCreator/src/components/characterSheetComponents/WeaponsComponent.jsx
@@ -6,129 +6,77 @@ import setLimits from "../../helperFunctions/setLimits";
 
 export default function WeaponsComponent({ id }) {
   const [character, setCharacter] = useState(getCharacterByID(id));
-  const [editedStats, setEditedStats] = useState({});
-  setLimits(id);
 
-  useEffect(() => {
-    if (character) {
-      setBaseStats(character);
-      setEditedStats(character.stats);
+  const guns = character.firearms || [];
+  const vehicles = character.vehicles || [];
+  const drones = character.drones || [];
+  const meleeWeapons = character.meleeWeapons || [];
+  const security = character.security || [];
+  const explosives = character.explosives || [];
+  const armor = character.armor || [];
+  const ammo = character.ammo || [];
+  const projectiles = character.projectiles || [];
+  const misc = character.misc || [];
+
+  let containedGuns = [];
+  //flatten gun objects and add parent key
+  Object.keys(guns).forEach((key) => {
+    if (typeof guns[key] === "object") {
+      Object.keys(guns[key]).forEach((subKey) => {
+        let gunObject = { ...guns[key][subKey], parentKey: key };
+        containedGuns.push(gunObject);
+      });
     }
-  }, [character]);
+  });
 
-  const setBaseStats = (character) => {
-    const statsToRender = [
-      "body",
-      "charisma",
-      "intuition",
-      "logic",
-      "reaction",
-      "strength",
-      "willpower",
-      "agility",
-    ];
+  console.log(containedGuns);
 
-    statsToRender.forEach((stat) => {
-      character.stats[stat].bonus = character.stats[stat].bonus ?? 0;
-      character.stats[stat].total =
-        character.stats[stat].total ??
-        parseInt(character.stats[stat]?.baseStats) +
-          parseInt(character.stats[stat]?.bonus);
-    });
-  };
+  //   return (
+  //     <div>
+  //       {containedGuns.map((key) => {
+  //         const stat = character.stats[key];
+  //         const baseStats = stat?.baseStats;
+  //         const bonus = stat?.bonus ?? 0;
+  //         const total = stat?.total ?? parseInt(baseStats) + parseInt(bonus);
+  //         const editedValue = editedStats[key]?.baseStats ?? baseStats;
+  //         const editedBonus = editedStats[key]?.bonus ?? bonus;
 
-  const handleBaseStatsChange = (key, newBaseValue, newBonusValue) => {
-    const updatedEditedStats = {
-      ...editedStats,
-      [key]: {
-        baseStats: newBaseValue,
-        bonus: newBonusValue,
-      },
-    };
-
-    const updatedStats = {
-      ...character.stats,
-      [key]: {
-        ...character.stats[key],
-        baseStats: newBaseValue,
-        bonus: newBonusValue,
-        total: parseInt(newBaseValue) + parseInt(newBonusValue),
-      },
-    };
-
-    const updatedCharacter = {
-      ...character,
-      stats: updatedStats,
-    };
-
-    setBaseStats(updatedCharacter);
-
-    updateCharacterInLocalStorage(id, updatedCharacter);
-    setCharacter(updatedCharacter);
-    setLimits(id);
-    console.log(character);
-
-    const event = new Event("characterAdded");
-    window.dispatchEvent(event);
-  };
-
-  const statsToRender = [
-    "body",
-    "charisma",
-    "intuition",
-    "logic",
-    "reaction",
-    "strength",
-    "willpower",
-    "agility",
-  ];
-
-  return (
-    <div>
-      {statsToRender.map((key) => {
-        const stat = character.stats[key];
-        const baseStats = stat?.baseStats;
-        const bonus = stat?.bonus ?? 0;
-        const total = stat?.total ?? parseInt(baseStats) + parseInt(bonus);
-        const editedValue = editedStats[key]?.baseStats ?? baseStats;
-        const editedBonus = editedStats[key]?.bonus ?? bonus;
-
-        return (
-          <div key={key}>
-            <h3>{key.charAt(0).toUpperCase() + key.slice(1)} Stats</h3>
-            {baseStats !== undefined ? (
-              <div>
-                <div>
-                  Base:
-                  <input
-                    type="number"
-                    value={editedValue}
-                    onChange={(e) => {
-                      handleBaseStatsChange(key, e.target.value, editedBonus);
-                    }}
-                  />
-                </div>
-                <div>
-                  Bonus:
-                  <input
-                    type="number"
-                    value={editedBonus}
-                    onChange={(e) => {
-                      handleBaseStatsChange(key, editedValue, e.target.value);
-                    }}
-                  />
-                </div>
-                <div>Total: {total}</div>
-                <DiceRoller total={total} />
-              </div>
-            ) : (
-              <div>
-                <p>Base Stats: Not available</p>
-              </div>
-            )}
-          </div>
-        );
-      })}
-    </div>
-  );
+  //         return (
+  //           <div key={key}>
+  //             <h3>{key.charAt(0).toUpperCase() + key.slice(1)} Stats</h3>
+  //             {baseStats !== undefined ? (
+  //               <div>
+  //                 <div>
+  //                   Base:
+  //                   <input
+  //                     type="number"
+  //                     value={editedValue}
+  //                     onChange={(e) => {
+  //                       handleBaseStatsChange(key, e.target.value, editedBonus);
+  //                     }}
+  //                   />
+  //                 </div>
+  //                 <div>
+  //                   Bonus:
+  //                   <input
+  //                     type="number"
+  //                     value={editedBonus}
+  //                     onChange={(e) => {
+  //                       handleBaseStatsChange(key, editedValue, e.target.value);
+  //                     }}
+  //                   />
+  //                 </div>
+  //                 <div>Total: {total}</div>
+  //                 <DiceRoller total={total} />
+  //               </div>
+  //             ) : (
+  //               <div>
+  //                 <p>Base Stats: Not available</p>
+  //               </div>
+  //             )}
+  //           </div>
+  //         );
+  //       })}
+  //     </div>
+  //   );
 }
