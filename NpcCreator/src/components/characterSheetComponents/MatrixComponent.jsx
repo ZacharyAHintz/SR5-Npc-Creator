5aabefc01dbba7b91af3d58891d83df865dab516
ZacharyAHintz <Zackhmail@gmail.com>
2024-07-28 22:37:28 -0500

    added RCCs and Cyberdecks

--- NpcCreator/src/components/characterSheetComponents/MatrixComponent.jsx
+++ NpcCreator/src/components/characterSheetComponents/MatrixComponent.jsx
@@ -5,11 +5,24 @@ export default function MatrixComponent({ id }) {
   const [character, setCharacter] = useState(getCharacterByID(id));
 
   const comlinks = character.comlinks || [];
+  const cyberdecks = character?.cyberdeck || [];
+  const rcc = character?.rcc || [];
 
   let comlinksArray = [];
 
   Object.keys(comlinks).forEach((key) => {
     comlinksArray.push(comlinks[key]);
+  });
+
+  let cyberdeckArray = [];
+
+  Object.keys(cyberdecks).forEach((key) => {
+    cyberdeckArray.push(cyberdecks[key]);
+  });
+  let rccArray = [];
+
+  Object.keys(rcc).forEach((key) => {
+    rccArray.push(rcc[key]);
   });
 
   return (
@@ -22,6 +35,46 @@ export default function MatrixComponent({ id }) {
           <div key={`comlinks-${index}`}>
             <p>{name}</p>
             <div>Rating: {rating}</div>
+          </div>
+        );
+      })}
+      <hr />
+
+      <h2>Cyberdecks</h2>
+
+      {cyberdeckArray.map((object, index) => {
+        const name = object.name;
+        const rating = object.rating;
+        const attributeArray = object.attributeArray;
+        const programs = object.programs;
+
+        return (
+          <div key={`cyberdecks-${index}`}>
+            <p>{name}</p>
+            <div>
+              Rating: {rating}, Attributes Array: {attributeArray}, Programs:{" "}
+              {programs}
+            </div>
+          </div>
+        );
+      })}
+      <hr />
+
+      <h2>RCC</h2>
+
+      {rccArray.map((object, index) => {
+        const name = object.name;
+        const rating = object.rating;
+        const dataProcessing = object.dataProcessing;
+        const firewall = object.firewall;
+
+        return (
+          <div key={`RCC-${index}`}>
+            <p>{name}</p>
+            <div>
+              Rating: {rating}, Data Processing: {dataProcessing}, Firewall:{" "}
+              {firewall}
+            </div>
           </div>
         );
       })}
